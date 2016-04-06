d69ee9d85c722c108de912b9f392491118c3b0a4
bondarenko <bondarenko@corp.sputnik.ru>
2016-04-07 02:51:35 +0300

    Added host flag

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/alyoshka/calc-server/server"
 )
 
+var host = flag.String("host", "localhost:8080", "Host and port to listen")
+
 func main() {
-	if err := server.Run("localhost:9090"); err != nil {
+	flag.Parse()
+	if err := server.Run(*host); err != nil {
 		log.Panic("error running server:", err)
 	}
 }
